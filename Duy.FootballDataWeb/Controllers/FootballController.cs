8a6e22c504bb87113ddf2ed887bf3363f1ad3c87
Duy IT <duy.it@outlook.com>
2018-05-17 08:51:44 +0700

    Add test grid

---
+++ Duy.FootballDataWeb/Controllers/FootballController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Duy.FootballData.Client;
+using Duy.FootballData.Common;
+using Duy.FootballData.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Duy.FootballDataWeb.Controllers
+{
+    public class FootballController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetLeagueTable()
+        {
+            var option = new FootballDataOption
+            {
+                ApiKey = string.Empty,
+                ResponseControl = ResponseControl.Full
+            };
+            IFootballDataClient client = new FootballDataClient(option);
+            LeagueTable leagueTable = await client.GetLeagueTable(445, null);
+
+            return Json(leagueTable);
+        }
+    }
+}
